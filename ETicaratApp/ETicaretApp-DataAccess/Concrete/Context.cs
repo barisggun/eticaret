32a0e09b8a9b59c5b0b926eab63e45cb849d8f2d
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-05-17 16:17:02 +0300

    controller

---
+++ ETicaratApp/ETicaretApp-DataAccess/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretApp_EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp_DataAccess.Concrete
+{
+    public class Context : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Connection string oluşturmak için bu yapıya ihtiyaç vardır.
+            optionsBuilder.UseSqlServer("server=ISILAY;database=ETicaretApp;integrated security=true; TrustServerCertificate = true ");
+        }
+
+        DbSet<Product> Products { get; set; }   
+    }
+}
