a68b71f8e9bfe6779d1a2972211233342908d61f
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-05-17 16:25:46 +0300

    sjafd

--- ETicaratApp/ETicaratApp.Panel.UI/Program.cs
+++ ETicaratApp/ETicaratApp.Panel.UI/Program.cs
@@ -7,7 +7,7 @@ namespace ETicaratApp.Panel.UI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
