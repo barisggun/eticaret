a091f0a7ce790c40f0596f885edd6efc6326f890
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-05-23 10:58:48 +0300

    deneme

--- ETicaratApp/ETicaratApp.Panel.UI/Controllers/ProductController.cs
+++ ETicaratApp/ETicaratApp.Panel.UI/Controllers/ProductController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ETicaratApp.Panel.UI.Controllers
 {
     public class ProductController : Controller
-    {
+    {//
         public IActionResult Index()
         {
             return View();
