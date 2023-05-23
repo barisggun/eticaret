a275389beb4a1d62bf853b4e652cb7871afd4084
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-05-23 16:22:53 +0300

    deneme

--- ETicaratApp/ETicaratApp.Panel.UI/Controllers/ProductController.cs
+++ ETicaratApp/ETicaratApp.Panel.UI/Controllers/ProductController.cs
@@ -1,13 +1,27 @@
 ﻿using ETicaretApp_BusinessLayer.Concrete;
+using ETicaretApp_DataAccess.Concrete;
 using ETicaretApp_DataAccess.EntityFramework;
+using ETicaretApp_EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaratApp.Panel.UI.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly Context context;
+
+        public ProductController(Context context, ProductManager productManager)
+        {
+            this.context = context;
+            this.productManager = productManager;
+        }
+
+        ProductManager productManager = new ProductManager
+            (new EfProductRepository());
+
         public IActionResult Index()
         {
+            List<Product> list = productManager.GetAll();
             return View();
         }
         
