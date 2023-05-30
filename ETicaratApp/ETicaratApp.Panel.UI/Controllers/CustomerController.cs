dd9810ca1826ed5d0726a3d8642a5fcbf3e5deec
Emirhan Temiz <emirhantmiz@gmail.com>
2023-05-30 17:12:21 +0300

    main sayfa giydirildi

---
+++ ETicaratApp/ETicaratApp.Panel.UI/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaratApp.Panel.UI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
