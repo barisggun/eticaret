644072da083c18edee3ddcfbd03e9d8d90947959
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-06-03 17:16:16 +0300

    CategoryList ViewComponent

--- ETicaratApp/ETicaratApp.Panel.UI/ViewComponents/Category/CategoryList.cs
+++ ETicaratApp/ETicaratApp.Panel.UI/ViewComponents/Category/CategoryList.cs
@@ -1,6 +1,18 @@
-﻿namespace ETicaratApp.Panel.UI.ViewComponents.Category
+﻿using ETicaretApp_BusinessLayer.Concrete;
+using ETicaretApp_DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaratApp.Panel.UI.ViewComponents.Category
 {
-    public class CategoryList
+    public class CategoryList:ViewComponent
     {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetAll();
+
+            return View(values);
+        }
     }
 }
