ecb6ec8e0359ac086c930656546826e4f2b0904e
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-06-03 15:46:40 +0300

    anasayfa product eklenmesi

--- ETicaratApp/ETicaratApp.Panel.UI/Controllers/MainController.cs
+++ ETicaratApp/ETicaratApp.Panel.UI/Controllers/MainController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ETicaretApp_BusinessLayer.Concrete;
+using ETicaretApp_DataAccess.EntityFramework;
+using ETicaretApp_EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaratApp.Panel.UI.Controllers
 {
     public class MainController : Controller
     {
+        ProductManager pm = new ProductManager(new EfProductRepository());
+        CategoryManager categoryManager = new CategoryManager
+            (new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+            List<Product> list = pm.GetProductListWithCategory();
+            return View(list);
+            //var values = pm.GetProductListWithCategory();
+            //return View(values);
         }
     }
 }
